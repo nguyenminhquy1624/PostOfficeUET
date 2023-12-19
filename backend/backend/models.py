0b6e5f56d79aae8883b44c8442267203fd5d3028
hoang <hoangldm2002hn@gmail.com>
2023-12-19 21:34:58 +0700

    setup

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 
 class UserAccount(AbstractUser):
-    username = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
-    First_Last_Name = models.CharField(max_length=255)
-    PhoneNumber = models.CharField(max_length=255)
-    email = models.EmailField(max_length=254, unique=True)
+    username = models.CharField(max_length=255, unique=True,null = True)
+    password = models.CharField(max_length=255,null = True)
+    First_Last_Name = models.CharField(max_length=255,null = True)
+    PhoneNumber = models.CharField(max_length=255,null = True)
+    email = models.EmailField(max_length=254, unique=True,null = True)
     ACCOUNT_TYPES = (
         (1, "Lanh Dao"),
         (2, "Truong Diem GD"),
@@ -16,4 +16,4 @@ class UserAccount(AbstractUser):
         (5, "Truong Diem Tap Ket"),
         (6, "Tap Ket Vien"),
     )
-    Type_Account = models.IntegerField(choices=ACCOUNT_TYPES)
+    Type_Account = models.IntegerField(choices=ACCOUNT_TYPES,null = True)
